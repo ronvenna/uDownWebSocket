f5e1fb1805e65a188cb13308a8a5baca50019c3c
Rohan Venapusala <rohanvenapusala@Rohans-MacBook-Pro.local>
2016-02-28 13:20:32 -0800

    removed some code for the rtm connecting twice

--- index.js
+++ index.js
@@ -171,35 +171,35 @@ var controller = app.configure(port, "23350178224.23361502390", "19938eaef76e57d
 
 // just a simple way to make sure we don't
 // connect to the RTM twice for the same team
-var _bots = {};
-function trackBot(bot) {
-  _bots[bot.config.token] = bot;
-}
+// var _bots = {};
+// function trackBot(bot) {
+//   _bots[bot.config.token] = bot;
+// }
 
-controller.on('create_bot',function(bot,config) {
+// controller.on('create_bot',function(bot,config) {
 
-  if (_bots[bot.config.token]) {
-    // already online! do nothing.
-  } else {
-    bot.startRTM(function(err) {
+//   if (_bots[bot.config.token]) {
+//     // already online! do nothing.
+//   } else {
+//     bot.startRTM(function(err) {
 
-      if (!err) {
-        trackBot(bot);
-      }
+//       if (!err) {
+//         trackBot(bot);
+//       }
 
-      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
-        if (err) {
-          console.log(err);
-        } else {
-          convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
-        }
-      });
+//       bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           convo.say('I am a bot that has just joined your team');
+//           convo.say('You must now /invite me to a channel so that I can be of use!');
+//         }
+//       });
 
-    });
-  }
+//     });
+//   }
 
-});
+// });
 
 controller.on('rtm_open', function (bot) {
     console.log('** The RTM api just connected!');
@@ -420,28 +420,28 @@ controller.hears(['.$'], 'direct_message,direct_mention,mention', function (bot,
     bot.startConversation(message, askIfTheyWantToMakeAnEvent.bind(this));
 });
 
-controller.storage.teams.all(function(err,teams) {
+// controller.storage.teams.all(function(err,teams) {
 
-  if (err) {
-    throw new Error(err);
-  }
+//   if (err) {
+//     throw new Error(err);
+//   }
 
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    console.log("TEAM", teams[t]);
-    if (teams[t].bot) {
-      var bot = controller.spawn(teams[t]).startRTM(function(err) {
-        if (err) {
-          console.log('Error connecting bot to Slack:',err);
-        } else {
-          console.log("tracking");
-          trackBot(bot);
-        }
-      });
-    }
-  }
+//   // connect all teams with bots up to slack!
+//   for (var t  in teams) {
+//     console.log("TEAM", teams[t]);
+//     if (teams[t].bot) {
+//       var bot = controller.spawn(teams[t]).startRTM(function(err) {
+//         if (err) {
+//           console.log('Error connecting bot to Slack:',err);
+//         } else {
+//           console.log("tracking");
+//           trackBot(bot);
+//         }
+//       });
+//     }
+//   }
 
-});
+// });
 
 
 // controller.hears(['dbdbhdhfs'], 'direct_message', function (bot, message) {
